e4c7abad27e38f5eaee8f1250cd7dffacac4596a
David Mawdsley <david.mawdsley@manchester.ac.uk>
2018-08-01 09:53:32 +0100

    Check for missing git: links when building

--- addlinks.sh
+++ addlinks.sh
@@ -6,11 +6,24 @@ taglist=$(git -C coursematerial/ tag)
 
 regex=""
 
+# Prepare a long regex containing all possible tags and their hashes
 for t in $taglist;
 do
 	commitid=$(git -C coursematerial/ rev-list -n 1 $t)
 	regex="${regex}s!(\[git:$t\])\(\)!\1\($gitrepoprefix$commitid\)!g;"
-#        sed -E "s!(\[git:$t\])\(\)!\1\($gitrepoprefix$commitid\)!g" $1
 done
 
+# Do the replacement 
 sed -E $regex $1 >$2
+
+# Check whether we have any [git:xxxx]() links that are 
+# still empty
+echo $2
+grep -E '\[git:.+\]\(\)' $2
+exit_status=$?
+if [ $exit_status -ne 1 ]; then 
+	echo "Unresolved links found"
+	exit 1
+
+fi
+
